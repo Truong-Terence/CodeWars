b61420c7a26b6e6e9674ed204eaca959722cf248
terence truong <tvn.terence@gmail.com>
2022-11-08 19:12:08 +0100

    sum_of_digits

---
+++ 8kyu/sum_of_digits.rb
@@ -0,0 +1,11 @@
+def digital_root(n)
+  # n = 1 + 2 + 3 + 4
+  while n > 9
+    n = n.to_s.split('').map(&:to_i).sum
+    # n = 1 + 2 + 3 + 4 = 10
+  end
+  n
+end
+
+# Shorter solution
+# n < 10 ? n : digital_root(n.digits.sum)
