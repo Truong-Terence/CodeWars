55956393f1a75b9aa744f09037c7d3935d8d75d7
terence truong <tvn.terence@gmail.com>
2022-11-08 18:09:47 +0100

    add comment to first kata

--- 8kyu/list_filtering.rb
+++ 8kyu/list_filtering.rb
@@ -1,3 +1,6 @@
+# Create a function that takes a list of non-negative integers and strings
+# and returns a new list with the strings filtered out.
+
 def filter_list(l)
 l.select{|i| i.is_a?(Integer)}
 end
