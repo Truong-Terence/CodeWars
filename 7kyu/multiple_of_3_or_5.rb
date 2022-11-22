7445882b0026aa62dc79cc0e44aee003e4ffae68
terence truong <tvn.terence@gmail.com>
2022-11-22 18:38:01 +0100

    modif range

--- 7kyu/multiple_of_3_or_5.rb
+++ 7kyu/multiple_of_3_or_5.rb
@@ -1,3 +1,3 @@
 def solution(number)
-  number < 0 ? 0 : (0..number-1).to_a.select { |n| n%3 == 0 || ( n%5 == 0 && n%3!=0 ) }.sum
+  number < 0 ? 0 : (1...number).to_a.select { |n| n%3 == 0 || ( n%5 == 0 && n%3!=0 ) }.sum
 end
